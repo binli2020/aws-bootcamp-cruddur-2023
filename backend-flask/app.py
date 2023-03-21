42adfb443bebe44b30d0b8603cb81031b424d92f
binli2020 <binli99@gmail.com>
2023-03-21 10:07:02 +0000

    Use decarator to instrut X-RAY

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -102,6 +102,7 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
+@xray_recorder.capture('home_endpoint')
 def data_home():
   data = HomeActivities.run()
   return data, 200
@@ -112,6 +113,7 @@ def data_notifications():
   return data, 200
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
+@xray_recorder.capture('user_endpoint')
 def data_handle(handle):
   model = UserActivities.run(handle)
   if model['errors'] is not None:
